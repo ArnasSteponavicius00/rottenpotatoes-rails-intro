e2d401ac7e962e1205952760668fbfbdd13057d0
ARNAS STEPONAVICIUS <g00361891@gmit.ie>
2021-03-07 21:13:55 +0000

    error fix attempt 4

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,7 +10,7 @@ class Movie < ActiveRecord::Base
     # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
     # movies with those ratings
     # if ratings_list is nil, retrieve ALL movies
-    if ratings.empty? or ratings.nil? 
+    if ratings && ratings.empty? or ratings.nil? 
        return Movie.all
     else
       return Movie.where(rating: ratings)
